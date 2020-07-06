5cecba7522aa1d8d0f670f65a307bc43ab93b08d
Gina Hase <noikedan00@gmail.com>
2020-07-07 04:54:44 +0900

    はじめてコミット

--- app.py
+++ app.py
@@ -1,66 +1,60 @@
-import json
-import os
+from flask import Flask, render_template
+from flask import request,send_file
+app = Flask(__name__)
 
-from flask import Flask, redirect, render_template, request
-
-application = Flask(__name__)
-
-DATA_FILE = "onelineChat.json"
-
-
-def save_data(name, memo):
-    """記録データを保存します
-     :param name: お名前
-     :type name: str
-
-    :param memo: メモ
-    :type memo: str
-    :return: None
-    """
-    try:
-
-        database = json.load(open(DATA_FILE, mode="r", encoding="utf-8"))
-    except FileNotFoundError:
-        database = []
-
-    database.insert(0, {
-        "name": name,
-        "memo": memo
-    })
-
-    json.dump(database, open(DATA_FILE, mode="w", encoding="utf-8"), indent=4, ensure_ascii=False)
-
-
-def load_data():
-    """記録データを返します"""
-    try:
-        database = json.load(open(DATA_FILE, mode="r", encoding="utf-8"))
-    except FileNotFoundError:
-        database = []
-    return database
-
-@application.route('/', methods=['POST'])
-def save():
+@app.route('/todos/uploader', methods=['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        input = './' + f.filename
+        output = './output.txt'
 
-        name = request.form.get('name')
-        memo = request.form.get('memo')
+        table = input.split('/')[-1].split('.')[0]
+        with open(input, encoding='utf-8') as f:
+            with open(output, 'w', encoding='utf-8') as g:
+                contents = "Insert into " + table + "("
+                i = 0
+                for row in f:
+                    if i == 0:
+                        typeList = row.rstrip().split(',')
+                    if i == 1:
+                        columList = row.rstrip().split(',')
+                        k = 0
+                        for c in columList:
+                            if len(columList) == k + 1:
+                                contents = contents + c + 'VALUES ('
+                            else:
+                                contents = contents + c + ','
+                            k = k + 1
+                        basecontets = contents
+                    if i >= 2:
+                        j = 0
+                        for r in row.rstrip().split(','):
+                            if not 'INTEGER' in typeList[j]:
+                                r = "'" + r + "'"
+                            if len(row.rstrip().split(',')) == j + 1:
+                                basecontets = basecontets + r
+                            else:
+                                basecontets = basecontets + r + ','
+                            j = j + 1
+                        basecontets = basecontets + ');' + '\n'
+                        g.write(basecontets)
+                        basecontets = contents
+                    i = i + 1
+            print("作成完了しました")
+    return render_template('index.html')
 
-        save_data(name , memo)
+@app.route('/download')
+def download_file():
 
-        return redirect('/')
+	path = './output.txt'
+	return send_file(path, as_attachment=True)
 
 
-@application.route('/')
+
+@app.route('/')
 def index():
-    """トッフぺージテンプレートを使用してぺージを表示します"""
-
-    rides = load_data()
-    return render_template('index.html', rides=rides)
-
-port = os.getenv('VCAP_APP_PORT', '5050')
+  return render_template('index.html')
 
 
-
-if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=int(port), debug=True)
